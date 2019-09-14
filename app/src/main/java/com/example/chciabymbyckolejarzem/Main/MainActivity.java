d7321d64baefc1fb9fa89a0c2f1d5072789849d0
Slavko <slawomirodka@gmai.com>
2019-09-14 17:37:52 +0200

    aktywnosci 3 bez funkcjonalnosci

--- app/src/main/java/com/example/chciabymbyckolejarzem/Main/MainActivity.java
+++ app/src/main/java/com/example/chciabymbyckolejarzem/Main/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.chciabymbyckolejarzem.R;
+import com.example.chciabymbyckolejarzem.loggingpage;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,10 +18,18 @@ public class MainActivity extends AppCompatActivity {
     private Button btn;
     private EditText nickname;
     public static final String NICKNAME = "usernickname";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    /*** Called when the user taps the Next button */
+    public void startbutton(View view) {
+        Intent intent = new Intent(this, loggingpage.class);
+        startActivity(intent);
+        // Do something in response to button
     }
 }

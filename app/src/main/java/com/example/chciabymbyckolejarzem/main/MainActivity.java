70a72e47db190fbf2391836ab98d563c431c33e0
szyborodziuk <szyborodziuk@gmail.com>
2019-09-14 17:48:48 +0200

    Refactor

--- app/src/main/java/com/example/chciabymbyckolejarzem/main/MainActivity.java
+++ app/src/main/java/com/example/chciabymbyckolejarzem/main/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.chciabymbyckolejarzem.main;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
